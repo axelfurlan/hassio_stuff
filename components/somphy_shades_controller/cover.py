ea6eb27ce3ae0ffa34371a50286c48fa9d986303
axelfurlan <axelfurlan@gmail.com>
2026-07-17 15:26:41 +0200

    Update cover.py to comply with ESPHome 2026.7.0, second attempt
    Previous attempt didn't work

--- components/somphy_shades_controller/cover.py
+++ components/somphy_shades_controller/cover.py
@@ -4,7 +4,6 @@ from esphome import automation
 from esphome.components import cover
 from esphome.const import (
     CONF_ID,
-    CONF_NAME,
 )
 from esphome.cpp_helpers import gpio_pin_expression
 
@@ -12,20 +11,21 @@ CONF_SHADE_CHANNEL = "shade_channel"
 
 somphy_shades_controller_ns = cg.esphome_ns.namespace("somphy_shades_controller")
 Somphy_Shades_Controller = somphy_shades_controller_ns.class_(
-    "Somphy_Shades_Controller", cover.Cover, cg.Component
+    "Somphy_Shades_Controller", cover.Cover, cg.Component)
+
+CONFIG_SCHEMA = (
+    cover.cover_schema(Somphy_Shades_Controller)
+    .extend(
+        {
+            cv.Required(CONF_SHADE_CHANNEL): cv.int_range(0, 5),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
 )
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(Somphy_Shades_Controller),
-        cv.Required(CONF_NAME): cv.string,
-        cv.Required(CONF_SHADE_CHANNEL): cv.int_range(0, 5),
-    }
-).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add(var.set_name(config[CONF_NAME]))
-    cg.add(var.set_channel(config[CONF_SHADE_CHANNEL]))
     await cover.register_cover(var, config)
+    cg.add(var.set_channel(config[CONF_SHADE_CHANNEL]))
